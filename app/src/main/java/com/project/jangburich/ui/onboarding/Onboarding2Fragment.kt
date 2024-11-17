b3921857c964263109492e71bf2c0dd108bd8ecb
PIYUJIN <yjyj020201@gmail.com>
2024-11-18 03:41:40 +0900

    [Feat] #5  두번째 onboarding 화면 구성

---
+++ app/src/main/java/com/project/jangburich/ui/onboarding/Onboarding2Fragment.kt
@@ -0,0 +1,41 @@
+package com.project.jangburich.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.jangburich.R
+import com.project.jangburich.databinding.FragmentOnboarding2Binding
+import com.project.jangburich.ui.MainActivity
+
+class Onboarding2Fragment : Fragment() {
+    lateinit var binding: FragmentOnboarding2Binding
+    lateinit var mainActivity: MainActivity
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentOnboarding2Binding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        binding.run {
+            buttonNext.setOnClickListener {
+                val nextFragment = Onboarding3Fragment()
+
+                val transaction = mainActivity.manager.beginTransaction()
+                transaction.replace(R.id.fragmentContainerView_main, nextFragment)
+                transaction.addToBackStack("")
+                transaction.commit()
+            }
+
+            buttonSkip.setOnClickListener {
+
+            }
+        }
+
+        return binding.root
+    }
+}
