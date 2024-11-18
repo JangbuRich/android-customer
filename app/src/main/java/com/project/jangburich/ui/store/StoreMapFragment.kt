2704df9afc4ab87c20abb2efa6f831a3bd1f053a
PIYUJIN <yjyj020201@gmail.com>
2024-11-19 03:25:09 +0900

    [Feat] #14  매장 찾기 - 지도 화면 tooltip 구성

--- app/src/main/java/com/project/jangburich/ui/store/StoreMapFragment.kt
+++ app/src/main/java/com/project/jangburich/ui/store/StoreMapFragment.kt
@@ -2,10 +2,12 @@ package com.project.jangburich.ui.store
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kakao.vectormap.KakaoMap
@@ -19,6 +21,12 @@ import com.project.jangburich.R
 import com.project.jangburich.databinding.FragmentStoreMapBinding
 import com.project.jangburich.ui.MainActivity
 import com.project.jangburich.ui.store.adapter.StoreCategoryAdapter
+import com.skydoves.balloon.ArrowOrientation
+import com.skydoves.balloon.ArrowPositionRules
+import com.skydoves.balloon.Balloon
+import com.skydoves.balloon.BalloonAnimation
+import com.skydoves.balloon.BalloonSizeSpec
+import com.skydoves.balloon.showAlignStart
 
 class StoreMapFragment : Fragment() {
 
@@ -53,6 +61,7 @@ class StoreMapFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         initAdapters()
+        showToolTip()
     }
 
     private fun initAdapters() {
@@ -72,6 +81,35 @@ class StoreMapFragment : Fragment() {
         binding.recyclerViewStoreCategory.apply {
             adapter = storeCategoryAdapter
             layoutManager = LinearLayoutManager(context, RecyclerView.HORIZONTAL, false)
+        }
+    }
+
+    private fun showToolTip() {
+        binding.run {
+            val balloon = Balloon.Builder(mainActivity)
+//                .setWidth(BalloonSizeSpec.WRAP)
+                .setWidthRatio(0.6f) // sets width as 60% of the horizontal screen's size.
+                .setHeight(BalloonSizeSpec.WRAP)
+                .setText("리스트로 매장을 확인할 수 있어요")
+                .setTextColorResource(R.color.gray_100)
+                .setTextSize(14f)
+                .setTextTypeface(ResourcesCompat.getFont(mainActivity, R.font.pretendard_medium)!!)
+                .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+                .setArrowOrientation(ArrowOrientation.END)
+                .setArrowSize(5)
+                .setArrowPosition(0.5f)
+                .setArrowColorResource(R.color.gray_20)
+                .setTextGravity(Gravity.CENTER)
+                .setElevation(0)
+                .setPaddingHorizontal(12)
+                .setPaddingVertical(8)
+                .setMarginHorizontal(5)
+                .setCornerRadius(8f)
+                .setBackgroundDrawableResource(R.drawable.background_tooltip)
+                .setBalloonAnimation(BalloonAnimation.ELASTIC)
+                .build()
+
+            buttonList.showAlignStart(balloon)
         }
     }
 
