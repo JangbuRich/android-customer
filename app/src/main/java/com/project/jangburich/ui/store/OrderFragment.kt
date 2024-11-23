cda2aa2f045c63f97c4e944f2049517cde97f196
PIYUJIN <yjyj020201@gmail.com>
2024-11-23 22:02:29 +0900

    [Feat] #24  메뉴 주문 - 메뉴 주문 화면 구성

---
+++ app/src/main/java/com/project/jangburich/ui/store/OrderFragment.kt
@@ -0,0 +1,38 @@
+package com.project.jangburich.ui.store
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.jangburich.R
+import com.project.jangburich.databinding.FragmentOrderBinding
+
+class OrderFragment : Fragment() {
+
+    lateinit var binding: FragmentOrderBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentOrderBinding.inflate(layoutInflater)
+
+        initView()
+
+        return binding.root
+    }
+
+    fun initView() {
+        binding.run {
+            toolbar.run {
+                buttonBack.setOnClickListener {
+                    fragmentManager?.popBackStack()
+                }
+                textViewTitle.text = "주문하기"
+            }
+        }
+    }
+
+}
