8dd1d6d7cc7ed1c479a04decd468b1687a3d5d38
PIYUJIN <yjyj020201@gmail.com>
2025-08-12 16:52:05 +0900

    [Feat] #5  기본 토스트 구성 및 기능 구현

--- app/src/main/java/com/project/jangburich/MainUtil.kt
+++ app/src/main/java/com/project/jangburich/MainUtil.kt
@@ -1,13 +1,18 @@
 package com.project.jangburich
 
 import android.app.Activity
+import android.content.res.Resources
 import android.os.Build
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import androidx.core.view.WindowCompat
+import kotlin.math.roundToInt
 
 object MainUtil {
+
+    fun Int.toPx(): Int = (this * Resources.getSystem().displayMetrics.density).roundToInt()
+
     // 투명한 status bar
     fun Activity.setStatusBarTransparent() {
         // 상태바를 투명하게 설정하고, 레이아웃을 상태바까지 확장
